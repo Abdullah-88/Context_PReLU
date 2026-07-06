ef3ee3f08a92a9b5fea05d4b2b4c81a01941080b
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-07 00:56:48 +0300

    Update context_prelu.py

--- context_prelu.py
+++ context_prelu.py
@@ -8,33 +8,26 @@ class MLP(nn.Module):
         self.proj_1 =  nn.Linear(dim,dim,bias=False)
         self.proj_2 =  nn.Linear(dim,dim,bias=False)        
         self.gelu = nn.GELU()
-       
-             	   
+                    	   
     def forward(self, x):
        
         x = self.proj_1(x)
         x = self.gelu(x)          
         x = self.proj_2(x)
-        
-                          
+                                  
         return x
         
-
 class Context_PReLUBlock(nn.Module):
 
     def __init__(self, d_model, num_tokens):
         super().__init__()
-       
-         
+                
         self.context_prelu = nn.PReLU(d_model * num_tokens)
         self.token_norm = nn.LayerNorm(d_model)
         self.local_mapping = MLP(d_model)
-                        
-        
+                                
     def forward(self, x):
-                  
-       
-        
+                                 
         residual = x
         
         x = self.token_norm(x)
@@ -43,8 +36,7 @@ class Context_PReLUBlock(nn.Module):
         dim1 = x.shape[1]
         dim2 = x.shape[2]
         context = x.reshape([dim0,dim1*dim2])
-        
-                       
+                               
         readout = self.context_prelu(context)        
         
         x = readout.reshape([dim0,dim1,dim2])
@@ -56,27 +48,17 @@ class Context_PReLUBlock(nn.Module):
         x = self.local_mapping(x)
                                           
         out = x + residual
-        
-        
+                
         return out
 
-
-
 class Context_PReLU(nn.Module):
-    def __init__(self, d_model,num_tokens, num_layers):
+    def __init__(self, d_model, num_tokens, num_layers):
         super().__init__()
         
         self.model = nn.Sequential(
-            *[Context_PReLUBlock(d_model,num_tokens) for _ in range(num_layers)]
+            *[Context_PReLUBlock(d_model, num_tokens) for _ in range(num_layers)]
         )
 
     def forward(self, x):
        
         return self.model(x)
-
-
-
-
-
-
-
